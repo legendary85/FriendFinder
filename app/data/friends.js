48127740c04b7148ac717a7d8367a5776c6945a7
Kourtney McCullough <kourtneyjmccullough@gmail.com>
2019-09-13 21:14:06 -0500

    friends array of objects add to data/friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friends = [
+  {
+    name: "Desiree",
+    photo:
+      "https://www.goldderby.com/wp-content/uploads/2018/11/Survivor-most-attractive-females-Desiree-Williams.jpg?w=620&h=360&crop=1",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    name: "Tammie",
+    photo:
+      "https://media1.popsugar-assets.com/files/thumbor/sS_9F_RBU68AKhLEykg2Upu4vtQ/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2019/02/06/865/n/1922729/tmp_SSakMu_c999e70e2043626b_20170822_Tamara_Pridgett_1267.jpg",
+    scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+  },
+  {
+    name: "Megan",
+    photo: "https://i.redd.it/psm5w26tmn731.jpg",
+    scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+  }
+];
+module.exports = friends;
