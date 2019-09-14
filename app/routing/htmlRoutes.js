07349d0de6d81ce7147b88f63ecb3c6b217df41a
Kourtney McCullough <kourtneyjmccullough@gmail.com>
2019-09-13 21:26:26 -0500

    Established html routes

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,12 @@
+var path = require("path");
+
+module.exports = function(app) {
+  //Displays the survey page
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname + "/../public/survey.html"));
+  });
+  //default, catch-all route that leads to home.html which displays the home page.
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname + "/../public/home.html"));
+  });
+};
